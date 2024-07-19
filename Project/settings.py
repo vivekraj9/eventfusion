8b2c26c684f2cc47ce1bbf0d5e9840dcd56b478e
vivekraj9 <vivek.raj@mitaoe.ac.in>
2024-07-19 12:16:43 +0530

    changed host

--- Project/settings.py
+++ Project/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-9vp@d9o)u-0i4&-(=-1=ly$y661shi%^(albjz-owr0ti-icj(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '*'
-]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
